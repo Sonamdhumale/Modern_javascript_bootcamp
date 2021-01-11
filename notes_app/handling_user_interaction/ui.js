6347e59ffb6fa38d1b7bd5afa7316c6d25ac59e7
Sonamdhumale <66209624+Sonamdhumale@users.noreply.github.com>
2021-01-11 18:05:52 +0530

    new

--- notes_app/handling_user_interaction/ui.js
+++ notes_app/handling_user_interaction/ui.js
@@ -7,18 +7,39 @@ const notes = [{
 }, {
     title : 'office modification',
     body : 'get a new seat',
-
-    
+   
 }]
+
+const filters = {
+    searchText : ' '
+}
+
+const renderNotes = function(notes, filters){
+    const filterNotes = notes.filter(function (note){
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ' '
+    
+    filterNotes.forEach(function(note){
+        const noteEle = document.createElement('p')
+        noteEle.textContent =note.title
+        document.querySelector('#notes').appendChild(noteEle)
+    })
+}
+renderNotes(notes, filters)
+
 document.querySelector('#create_note').addEventListener('click', function(e){
     e.target.textContent = 'button was clicked'
     //console.log('did this work ?')
 })
 
-document.querySelectorAll('#remove_all')[1].addEventListener('click',function(){
-    document.querySelectorAll('.note').forEach('click', function(note){
-        note.remove()
-        
-    })
+
+document.querySelector('#search_text').addEventListener('input',function(e){
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
 
+document.querySelector('#for_fun').addEventListener('change', function(e){
+    console.log(e.target.checked)
+})
--- todo_app/todo_challenge/todo_challenge.js
+++ todo_app/todo_challenge/todo_challenge.js
@@ -15,22 +15,66 @@ const todo = [{
     completed : true
 }]
 
-const inCompleteTodos = todo.filter(function(todos){
-    return !todos.completed 
-})
+const filters = {
+    searchText : '',
+    hide_completed : false
+}
 
-const summery = document.createElement('h2')
-summery.textContent = `You have ${inCompleteTodos.length} todos left`
-document.querySelector('body').appendChild(summery)
+const renderedTodos = function(todos , filters){
+    let filteredTodos = todos.filter(function(todo){
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
 
-todo.forEach(function(todos){
-    const p = document.createElement('p')
-    p.textContent = todos.text
-    document.querySelector('body').appendChild(p)
-})
+    filteredTodos  = filteredTodos.filter(function(todo){
+        if(filters.hide_completed){
+                return !todo.completed
+        }else{
+            return true
+        }
+    })
+
+    const inCompleteTodos = filteredTodos.filter(function(todo){
+        return !todo.completed 
+    })
+    
+    document.querySelector('#todos').innerHTML= ''
+
+    const summery = document.createElement('h2')
+    summery.textContent = `You have ${inCompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summery)
+    
+    filteredTodos.forEach(function(todos){
+        const p = document.createElement('p')
+        p.textContent = todos.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+
+renderedTodos(todo, filters)
+
+
 
 //todo creation
-document.querySelector('button').addEventListener('click',function(e)
-{
-    console.log('Add a new todo')
+
+
+
+
+document.querySelector('#search_text').addEventListener('input', function(e){
+    filters.searchText = e.target.value
+    renderedTodos(todo, filters)
+})
+
+document.querySelector('#new_todo').addEventListener('submit', function(e){
+    e.preventDefault()
+    todo.push({
+        text : e.target.elements.text.value,
+        completed : false
+    })
+    renderedTodos(todo, filters)
+    e.target.elements.text.value = ''
+})
+
+document.querySelector('#hide_completed').addEventListener('change', function(e){
+    filters.hide_completed=e.target.checked
+    renderedTodos(todo, filters)
 })
