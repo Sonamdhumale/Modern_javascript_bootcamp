17cf67a31ca7780dc7b9c4c2ce8ab4c61af53853
Sonamdhumale <66209624+Sonamdhumale@users.noreply.github.com>
2021-01-14 18:09:35 +0530

    gr

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,10 +1,23 @@
 const Hangman = function(word , remaining){
-    this.word = word
+    this.word = word.toLowerCase().split(' ')
     this.remaining = remaining
+    this.guessedLetters = ['c']
+}
+Hangman.prototype.getPuzzle = function(){
+    let puzzle = ' '
+
+    this.word.forEach((letter)=>{
+        if(this.guessedLetters.includes(letter)){
+            puzzle = puzzle + letter
+        }else{
+            puzzle = puzzle + '*'
+        }
+    })
+    return puzzle
 }
 
 const game1 = new Hangman('cat' ,2)
 console.log(game1)
 
-const game2 = new Hangman('India' , 4)
+const game2 = new Hangman('new jersey' , 4)
 console.log(game2)
--- oop/person.js
+++ oop/person.js
@@ -1,29 +1,31 @@
-const Person = function(firstName, lastName, age, likes = []){
+// Prototypal Inheritance
+
+const Person = function (firstName, lastName, age, likes = []) {
     this.firstName = firstName
     this.lastName = lastName
     this.age = age
-    this.likes = this.likes
+    this.likes = likes
 }
 
-Person.prototype.getBio = function(){
-    let bio = `${this.firstName} is ${this.age}`
+Person.prototype.getBio = function () {
+    let bio = `${this.firstName} is ${this.age}.`
 
     this.likes.forEach((like) => {
-        bio += `${this.firstName} likes ${this.likes}`
+        bio += ` ${this.firstName} likes ${like}.`
     })
 
     return bio
 }
 
-Person.prototype.setName = function(fullName){
-    const  names = fullName.split(' ')
+Person.prototype.setName = function (fullName) {
+    const names = fullName.split(' ')
     this.firstName = names[0]
     this.lastName = names[1]
 }
 
-const me = new Person('sonam' , 'dhumale' , '22',['singing' , 'coocking'])
-me.setName('abc Dhumale')
+const me = new Person('sonam', 'dhumale', 22, ['cooking', 'singing'])
+me.setName('priti dhumale')
 console.log(me.getBio())
 
-const person2 = new Person('priti' , 'dhumale' , '20' , [])
+const person2 = new Person('abc', 'aaa', 20)
 console.log(person2.getBio())
